62d109abdaaf0b186d156e921f4e037f82c44095
Eef-M <98412601+Eef-M@users.noreply.github.com>
2024-02-18 19:31:57 +0700

    Push to Github

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
+const Dashboard = () => {
+
+    const navigate = useNavigate();
+
+    const { user, userInfo } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if (!user) {
+            navigate('/login')
+        }
+    }, [user])
+
+    return (
+        <div>
+            <h1>Welcome, {userInfo.first_name}</h1>
+        </div>
+    )
+}
+
+export default Dashboard
